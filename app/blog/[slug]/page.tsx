34b3715efafe2c0f3141cda05a199bff602cd130
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2025-11-17 07:42:15 +0900

    fix: 記事詳細ページのコードスタイルを改善し、pre要素にパディングを追加

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -151,7 +151,7 @@ export default async function BlogPost({ params }: PageProps) {
                 prose-p:text-gray-700 prose-p:leading-relaxed
                 prose-a:text-blue-600 prose-a:no-underline hover:prose-a:underline
                 prose-code:text-pink-600 prose-code:bg-gray-100 prose-code:px-1 prose-code:py-0.5 prose-code:rounded prose-code:before:content-[''] prose-code:after:content-['']
-                prose-pre:bg-gray-900 prose-pre:text-gray-100 prose-pre:overflow-x-auto
+                prose-pre:bg-gray-900 prose-pre:text-gray-100 prose-pre:overflow-x-auto prose-pre:p-1
                 prose-ul:list-disc prose-ul:pl-6
                 prose-ol:list-decimal prose-ol:pl-6
                 prose-li:text-gray-700
